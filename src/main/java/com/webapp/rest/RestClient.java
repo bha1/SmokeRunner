5ce3dc839baa7d27d20cf6b68c5cdeb08628f923
bha1 <peeyushbhawan@gmail.com>
2016-12-19 00:40:14 +1100

    xlsx read and write complete !

--- src/main/java/com/webapp/rest/RestClient.java
+++ src/main/java/com/webapp/rest/RestClient.java
@@ -1,59 +1,57 @@
 package com.webapp.rest;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.commons.io.IOUtils;
 
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/client")
 public class RestClient {
 
-	@SuppressWarnings("resource")
-	@GET
+	@POST
 	@Path("/excel")
 	@Produces("application/vnd.ms-excel")
-	public Response processExcel(){
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response processExcel(@FormDataParam("uploadFile") InputStream fileInputStream,
+            @FormDataParam("uploadFile") FormDataContentDisposition fileFormDataContentDisposition){
 		ResponseBuilder responseBuilder = null;
 		Response response = null;
 		    File tempFile;
 			try {
-				tempFile = File.createTempFile("sample", "xlsx");
-System.out.println("step 1");
+			tempFile = File.createTempFile("sample", "xlsx");
 		    tempFile.deleteOnExit();
 		    System.out.println("step 2");
 		    FileOutputStream out = new FileOutputStream(tempFile);
-		    System.out.println("step 3");
-		    InputStream in = getClass().getClassLoader().getResourceAsStream(".");
-		    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		    String resource;
-
-		      while( (resource = br.readLine()) != null ) {
-		        System.out.println( resource );
-		      }
+		    
+		    //InputStream in = getClass().getClassLoader().getResourceAsStream(".");
+	//	    BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		   // throw new FileNotFoundException("file not found"+getClass().getClassLoader().getResourceAsStream("REST_API.xlsx"));
-		    IOUtils.copy(getClass().getClassLoader().getResourceAsStream("REST_API.xlsx"), out);
+		   // IOUtils.copy(getClass().getClassLoader().getResourceAsStream("REST_API.xlsx"), out);
+		    System.out.println("step 3");
+		    System.out.println(fileInputStream);
+		    System.out.println(fileFormDataContentDisposition.getFileName());
+		    IOUtils.copy(fileInputStream, out);
 		    System.out.println("step 4");
 			responseBuilder = Response.ok((Object)tempFile);
+			responseBuilder.header("Content-Disposition",
+					"attachment; filename=\"sample.xlsx\"");
 			response = responseBuilder.build();
-			System.out.println("step 5");
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-//			response.header("Content-Disposition",
-//					"attachment; filename=new-excel-file.xls");
 		return response;
 	}
 	
