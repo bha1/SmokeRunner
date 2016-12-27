035285b41f9474553b20f54b36ac6ce984fcd9ac
bha1 <peeyushbhawan@gmail.com>
2016-12-27 18:34:34 +1100

    get and delete request throws null pointer #1

--- src/main/java/com/smoke/service/SmokeHttpRequestBuilder.java
+++ src/main/java/com/smoke/service/SmokeHttpRequestBuilder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.http.protocol.HTTP;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -42,7 +43,9 @@ public class SmokeHttpRequestBuilder {
 		SmokeHttpRequestDTO requestDTO = new SmokeHttpRequestDTO();
 		requestDTO.setUrl(row.getCell(0).getRichStringCellValue().getString());
 		requestDTO.setHttpRequestType(HTTPRequestType.fromValue(row.getCell(1).getRichStringCellValue().getString()));
-		requestDTO.setRequestPayload(row.getCell(2).getRichStringCellValue().getString());
+		if(HTTPRequestType.POST.equals(requestDTO.getHttpRequestType()) || HTTPRequestType.PUT.equals(requestDTO.getHttpRequestType())){
+			requestDTO.setRequestPayload(row.getCell(2).getRichStringCellValue().getString());
+		}
 		// TODO will do some thing about this, soon
 		//requestDTO.setHeaders(row.getCell(0).toString());
 		return requestDTO;
